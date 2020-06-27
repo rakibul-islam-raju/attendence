ab9b10e34f472947989ea3e542296b6f13785f97
Rakibul Islam <rakibul.islam7772588@gmail.com>
2020-06-27 20:45:47 +0600

    static

--- attendance/settings.py
+++ attendance/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,12 +132,16 @@ WHITENOISE_MIMETYPES = {
     '.xsl': 'application/xml'
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "static"),
     '/static/',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
